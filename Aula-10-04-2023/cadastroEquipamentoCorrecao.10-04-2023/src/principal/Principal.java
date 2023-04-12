6681cd7d49fa7f04ff625d677324982c85b51d00
unknown <carbonaropedro@gmail.com>
2023-04-11 21:45:06 -0300

    Exercicio aula 11/04/23

---
+++ Aula-10-04-2023/cadastroEquipamentoCorrecao.10-04-2023/src/principal/Principal.java
@@ -0,0 +1,17 @@
+package principal;
+
+
+import telas.ParteGrafica;// Importando o objeto
+
+public class Principal {
+
+	
+	public static void main(String[] args)
+	{
+					
+		ParteGrafica paGrafica = new ParteGrafica();
+						 
+		paGrafica.menu();
+		
+	}
+}
